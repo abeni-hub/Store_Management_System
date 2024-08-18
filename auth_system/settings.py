43cf03b4887d91d5f9f24e544c7ae3e42ab0f14e
Abenezer Sileshi <138670844+abeni-hub@users.noreply.github.com>
2024-08-18 14:06:04 +0300

    Update settings.py

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u161g!7kb05@ou5x+nmtz0rg(zp+saa!wswhj-vlw1x_2399h='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','maroelectronics.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
