e66a191adbbd65edfbac1c0ac9ee471b127f3db8
Abenezer Sileshi <138670844+abeni-hub@users.noreply.github.com>
2025-08-19 03:52:43 -0700

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.utils import timezone
 import datetime
 
-
 class Role(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
@@ -18,7 +17,6 @@ class Role(models.Model):
     @classmethod
     def get_role_by_name(cls, name):
         return cls.objects.filter(name=name).first()
-
 
 class UserAccountManager(BaseUserManager):
     def create_user(self, email, name, password=None, **extra_fields):
@@ -46,7 +44,6 @@ class UserAccountManager(BaseUserManager):
     #
     #     return self.create_user(email, password, **extra_fields)
 
-
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
@@ -72,8 +69,7 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     def remove_groups(self):
         self.groups.clear()
-
-
+    
 class Cashier(UserAccount):
     phone_number = models.CharField(max_length=15, unique=True)
 
@@ -88,7 +84,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
 class SubCategory(models.Model):
     name = models.CharField(max_length=255)
     main_category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
@@ -96,37 +91,96 @@ class SubCategory(models.Model):
     def __str__(self):
         return self.name
 
-
 class Electronics(models.Model):
     main_category = models.ForeignKey(Category, related_name='electronics', on_delete=models.CASCADE)
     sub_category = models.ForeignKey(SubCategory, related_name='electronics', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     size = models.CharField(max_length=50)
     quantity = models.PositiveIntegerField()  # Represents stock
+    added_by = models.CharField(max_length=100 , null= True)
+
     buying_price = models.DecimalField(max_digits=10, decimal_places=2)
     date_added = models.DateField(default=datetime.date.today)
+    
 
     # selling_price = models.DecimalField(max_digits=10, decimal_places=2)  # Added selling price for clarity
     # Add any other relevant fields
 
     def __str__(self):
         return self.name
+ 
+class Buying(models.Model):
+    main_category = models.ForeignKey(Category, related_name='buying', on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(SubCategory, related_name='buying', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    size = models.CharField(max_length=50)
+    quantity = models.PositiveIntegerField()  # Represents stock
+    added_by = models.CharField(max_length=100 , null= True)
 
+    buying_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date_added = models.DateField(default=datetime.date.today)
+    
 
+    # selling_price = models.DecimalField(max_digits=10, decimal_places=2)  # Added selling price for clarity
+    # Add any other relevant fields
+
+    def __str__(self):
+        return self.name
+ 
 class Sales(models.Model):
     item_name = models.CharField(max_length=255)
     main_category = models.ForeignKey(Category, related_name='sales', on_delete=models.CASCADE)
     sub_category = models.ForeignKey(SubCategory, related_name='sales', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    date = models.DateField()
+    size = models.CharField(max_length=50 ,null=True)
+
+    date = models.DateField()  
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    credit_amount = models.DecimalField(max_digits=10, decimal_places=2 , null=True)
     seller_name = models.CharField(max_length=255)
     profit = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    commission_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    buyer_name = models.CharField(max_length=255 ,null=True, blank=True)
+    status = models.CharField(null=True, max_length=50, choices=[
+        ('paid', 'Paid'),
+        ('unpaid', 'Unpaid'),
+       
+    ])  
+    payment_method = models.CharField(max_length=50, choices=[
+        ('cash', 'Cash'),
+        ('transfer', 'Transfer'),
+        ('debit', 'Debit'),
+        ('telebirr', 'telebirr')
+    ], default='cash')  
 
     def __str__(self):
         return self.item_name
+class Exchange(models.Model):
+    item_name = models.CharField(max_length=255)
+    main_category = models.ForeignKey(Category, related_name='exchange_main_category', on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(SubCategory, related_name='exchange_sub_category', on_delete=models.CASCADE)
+    size = models.CharField(max_length=50 ,null=True)
+    new_item_size = models.CharField(max_length=50 ,null=True)
 
+    new_item_name = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField(default=1)
+    date = models.DateField(default=timezone.now)
+    new_item_main_category = models.ForeignKey(Category, related_name='exchange_new_main_category', on_delete=models.CASCADE, null=True, blank=True)
+    new_item_sub_category = models.ForeignKey(SubCategory, related_name='exchange_new_sub_category', null=True,on_delete=models.CASCADE)
+    estimated_exchange_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    additional_payment = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    seller_name = models.CharField(max_length=255)
+    profit = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    commission_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    payment_method = models.CharField(max_length=50, choices=[
+        ('cash', 'Cash'),
+        ('transfer', 'Transfer'),
+        ('debit', 'Debit'),
+        ('telebirr', 'telebirr')
+    ], default='cash')
 
+    def __str__(self):
+        return self.item_name
 class SalesSummary(models.Model):
     total_sales = models.DecimalField(max_digits=10, decimal_places=2)
     total_profit = models.DecimalField(max_digits=10, decimal_places=2)
@@ -135,7 +189,6 @@ class SalesSummary(models.Model):
     def __str__(self):
         return f"Summary for {self.date}"
 
-
 class Expense(models.Model):
     name = models.CharField(max_length=255)  # Expense name
     amount = models.DecimalField(max_digits=10, decimal_places=2)  # Amount
@@ -143,5 +196,16 @@ class Expense(models.Model):
     description = models.TextField(blank=True, null=True)  # Optional description
     is_verified = models.BooleanField(default=False)  # Verification status
 
+    def __str__(self):
+        return self.name
+
+class Revenue(models.Model):
+    name = models.CharField(max_length=255)  # Expense name
+    recievername = models.CharField(max_length=255)  # Expense name
+    amount = models.DecimalField(max_digits=10, decimal_places=2)  # Amount
+    date = models.DateField()  # Date of the expense
+    description = models.TextField(blank=True, null=True)  # Optional description
+    is_verified = models.BooleanField(default=False)  # Verification status
+    reason = models.CharField(max_length=255 ,null=True, blank=True)
     def __str__(self):
         return self.name
